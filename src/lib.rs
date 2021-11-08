d7f087c76f0f60c24b1ebdb180891b4259bfa0ae
Saecki <tobiasschmitz2001@gmail.com>
2021-11-08 04:20:13 +0100

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,169 @@
+use std::cmp::max;
+use std::collections::HashMap;
+
+pub struct Tree<'a> {
+    input: &'a str,
+    char_count: HashMap<char, usize>,
+    char_code: HashMap<char, String>,
+    node: Option<Node>,
+}
+
+impl<'a> Tree<'a> {
+    pub fn from(input: &'a str) -> Self {
+        // count chars
+        let mut char_count = HashMap::<char, usize>::new();
+        for c in input.chars() {
+            match char_count.get_mut(&c) {
+                Some(v) => {
+                    *v += 1;
+                }
+                None => {
+                    char_count.insert(c, 1);
+                }
+            }
+        }
+
+        // create nodes
+        let mut nodes: Vec<_> = char_count
+            .iter()
+            .map(|(k, v)| {
+                Node::Leaf(LeafNode {
+                    char: *k,
+                    count: *v,
+                })
+            })
+            .collect();
+
+        // construct tree
+        while nodes.len() > 1 {
+            nodes.sort_by_key(|n| n.count());
+
+            let a = nodes.remove(0);
+            let b = nodes.remove(0);
+            let n = BranchNode {
+                count: a.count() + b.count(),
+                a: Box::new(a),
+                b: Box::new(b),
+            };
+            nodes.push(Node::Branch(n));
+        }
+
+        // encode chars
+        let node = nodes.into_iter().next();
+        let mut char_code = HashMap::new();
+        if let Some(n) = &node {
+            n.encode_chars(String::new(), &mut char_code);
+        }
+
+        Self {
+            input,
+            char_count,
+            char_code,
+            node,
+        }
+    }
+
+    pub fn width(&self) -> usize {
+        self.node.as_ref().map_or(0, |n| n.width())
+    }
+
+    pub fn height(&self) -> usize {
+        self.node.as_ref().map_or(0, |n| n.height())
+    }
+
+    pub fn char_count(&self) -> &HashMap<char, usize> {
+        &self.char_count
+    }
+
+    pub fn char_code(&self) -> &HashMap<char, String> {
+        &self.char_code
+    }
+
+    pub fn node(&self) -> Option<&Node> {
+        self.node.as_ref()
+    }
+
+    pub fn encode(&self, sep: &str) -> String {
+        let mut output = String::new();
+        let mut iter = self.input.chars().peekable();
+        while let Some(c) = iter.next() {
+            if let Some(s) = self.char_code.get(&c) {
+                output.push_str(s);
+                if iter.peek().is_some() {
+                    output.push_str(sep)
+                }
+            }
+        }
+        return output;
+    }
+}
+
+#[derive(Debug)]
+pub enum Node {
+    Branch(BranchNode),
+    Leaf(LeafNode),
+}
+
+impl Node {
+    pub fn count(&self) -> usize {
+        match self {
+            Self::Branch(b) => b.count,
+            Self::Leaf(l) => l.count,
+        }
+    }
+
+    pub fn width(&self) -> usize {
+        match self {
+            Self::Branch(n) => 1 + n.a.width() + n.b.width(),
+            Self::Leaf(_) => 1,
+        }
+    }
+
+    pub fn left_width(&self) -> usize {
+        match self {
+            Self::Branch(n) => n.a.width(),
+            Self::Leaf(_) => 0,
+        }
+    }
+
+    pub fn right_width(&self) -> usize {
+        match self {
+            Self::Branch(n) => n.b.width(),
+            Self::Leaf(_) => 0,
+        }
+    }
+
+    pub fn height(&self) -> usize {
+        match self {
+            Self::Branch(n) => max(n.a.height(), n.b.height()) + 1,
+            Self::Leaf(_) => 1,
+        }
+    }
+
+    fn encode_chars(&self, mut path: String, table: &mut HashMap<char, String>) {
+        match self {
+            Self::Branch(n) => {
+                let path_a = format!("{}{}", path, '0');
+                n.a.encode_chars(path_a, table);
+                path.push('1');
+                n.b.encode_chars(path, table);
+            }
+            Self::Leaf(n) => {
+                table.insert(n.char, path);
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct BranchNode {
+    pub count: usize,
+    pub a: Box<Node>,
+    pub b: Box<Node>,
+}
+
+#[derive(Debug)]
+pub struct LeafNode {
+    pub char: char,
+    pub count: usize,
+}
---
+++ src/main.rs
@@ -0,0 +1,152 @@
+use std::cmp::{max, min};
+use std::process::exit;
+
+use huffman::{Node, Tree};
+
+fn main() {
+    let mut args = std::env::args().skip(1);
+    let input = match args.next() {
+        Some(s) => s,
+        None => {
+            eprintln!("No input string");
+            exit(1);
+        }
+    };
+
+    let tree = Tree::from(&input);
+
+    println!();
+    println!("Character count:");
+    for (c, n) in tree.char_count() {
+        println!("{}: {}", c, n);
+    }
+
+    println!();
+    println!("Character code:");
+    for (c, e) in tree.char_code() {
+        println!("{}: {}", c, e);
+    }
+
+    let mut canvas = Canvas::new();
+    if let Some(n) = tree.node() {
+        paint_tree(&mut canvas, n, 0, n.left_width());
+    }
+    println!();
+    println!("Tree:");
+    for l in &canvas.lines {
+        println!("{}", l);
+    }
+
+    println!();
+    println!("Encoded:");
+    println!("{}", tree.encode(" "));
+}
+
+struct Canvas {
+    lines: Vec<String>,
+}
+
+impl Canvas {
+    fn new() -> Self {
+        Self { lines: Vec::new() }
+    }
+
+    fn char(&mut self, x: usize, y: usize, char: char) {
+        let line = match self.lines.get_mut(y) {
+            Some(l) => l,
+            None => {
+                for _ in self.lines.len()..=y {
+                    self.lines.push(String::new());
+                }
+                &mut self.lines[y]
+            }
+        };
+
+        if line.len() <= x {
+            for _ in line.len()..x {
+                line.push(' ');
+            }
+            line.push(char);
+        } else {
+            let mut byte_index = None;
+            for (i, (b, _)) in line.char_indices().enumerate() {
+                if i == y {
+                    byte_index = Some(b);
+                    break;
+                }
+            }
+            if let Some(b) = byte_index {
+                line.remove(b);
+                line.insert(b, char);
+            }
+        }
+    }
+
+    fn string(&mut self, x: usize, y: usize, string: &str) {
+        let line = match self.lines.get_mut(y) {
+            Some(l) => l,
+            None => {
+                for _ in self.lines.len()..=y {
+                    self.lines.push(String::new());
+                }
+                &mut self.lines[y]
+            }
+        };
+
+        if line.len() < x {
+            for _ in line.len()..x {
+                line.push(' ');
+            }
+            line.push_str(string);
+        } else {
+            for (i, c) in string.chars().enumerate() {
+                self.char(x + i, y, c);
+            }
+        }
+    }
+
+    fn left_line(&mut self, x: usize, y1: usize, y2: usize) {
+        let ys = min(y1, y2);
+        let ye = max(y1, y2);
+
+        let mut xo = ye - ys + 1;
+        for y in ys..=ye {
+            xo -= 1;
+            self.char(x + xo, y, '/');
+        }
+    }
+
+    fn right_line(&mut self, x: usize, y1: usize, y2: usize) {
+        let ys = min(y1, y2);
+        let ye = max(y1, y2);
+
+        let mut xo = 0;
+        for i in ys..=ye {
+            self.char(x + xo, i, '\\');
+            xo += 1;
+        }
+    }
+}
+
+fn paint_tree(canvas: &mut Canvas, node: &Node, x: usize, y: usize) {
+    match node {
+        Node::Branch(n) => {
+            let y1 = y - 1 - n.a.right_width();
+            let y2 = y - 1;
+            let ax = x + (y2 - y1) + 2;
+            canvas.left_line(x + 1, y1, y2);
+            paint_tree(canvas, &n.a, ax, y1);
+
+            canvas.string(x, y, &n.count.to_string());
+
+            let y1 = y + 1;
+            let y2 = y + 1 + n.b.left_width() * 1;
+            let ax = x + (y2 - y1) + 2;
+            canvas.right_line(x + 1, y1, y2);
+            paint_tree(canvas, &n.b, ax, y2);
+        }
+        Node::Leaf(n) => {
+            canvas.string(x, y, &format!("{} -- {}", n.count.to_string(), n.char));
+        }
+    }
+}
